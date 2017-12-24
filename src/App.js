0d88ce6f06d7e33ef8e20377da70c9c8afdff2b6
uanoob <usnet@ukr.net>
2017-12-24 10:49:29 +0200

    Re-Using Components, Dynamic Content, Working With Props, Children Property

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Person name="Max" age="29" />
+       <Person name="Manu" age="27">My hobbies: racing</Person>
+       <Person name="Stephanie" age="26"/>
       </div>
     );
   }
