6e47880c48c673f97501f429f26867e9766413e5
uanoob <usnet@ukr.net>
2017-12-24 13:07:57 +0200

    Two Way Binding, Inline Styles

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler = (newName) => {
-    console.log("Name switched");
+  switchNameHandler = newName => {
     this.setState({
       persons: [
         { name: newName, age: 28 },
@@ -22,26 +21,49 @@ class App extends Component {
     });
   };
 
+  nameChangeHandler = event => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: event.target.value, age: 26 }
+      ]
+    });
+  };
+
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!!')}>Switch Name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler("Maximilian!!!")}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          change={this.nameChangeHandler}
         />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
+          click={this.switchNameHandler.bind(this, "Max!")}
+          change={this.nameChangeHandler}
         >
           My hobbies: racing
         </Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
+          change={this.nameChangeHandler}
         />
       </div>
     );
